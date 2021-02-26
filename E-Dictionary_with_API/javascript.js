f4275f688c83575340cc28014c88fbf061d6835b
Rio Joy Cabalda <rjcabalda@gmail.com>
2021-02-27 00:49:09 +0800

    update Dictionary App

--- E-Dictionary_with_API/javascript.js
+++ E-Dictionary_with_API/javascript.js
@@ -10,7 +10,8 @@ const def = document.querySelector('.def');
 const vg = document.querySelector('.vg');
 const other_word = document.querySelector('.other_word_anchor');
 const sentence = document.querySelector('.sentence');
-const Display = document.querySelector('.Display');
+const display_div = document.querySelector('.display_div');
+
 
 function clear() {
     hword.innerHTML = '';
@@ -23,142 +24,140 @@ function clear() {
     vg.innerHTML = '';
     other_word.innerHTML = '';
     sentence.innerHTML = '';
-    Display.innerHTML = '';
+    display_div.innerHTML = '';
 }
 function errorDisplay(word) {
-    Display?.classList.remove('invisible');
+    display_div.classList.remove('invisible');
     let display = `
     <h1 class="break-text breakText"><em>${word}</e> is not in the system</h1>
     <p>Sorry, the word you’re looking for can’t be found in the dictionary.</p>
     `;
-    Display.innerHTML = display;
+    display_div.innerHTML = display;
 
 }
 function loading() {
-    Display?.classList.remove('invisible');
+    display_div.classList.remove('invisible');
     let loadingProgress =
         `<div class="loadingContainer">
                 <div class="spinner-border text-primary" role="status">
                     <span class="sr-only">Loading...</span>
                 </div>
         </div>`;
-    Display.innerHTML = loadingProgress;
+        display_div.innerHTML = loadingProgress;
 }
 function maintenanceDisplay() {
-    Display?.classList.remove('invisible');
+    display_div.classList.remove('invisible');
     let display = `
     <h1 class='alert alert-warning'>System Error</h1>
     <p class="alert alert-danger">Ops, something went wrong!</p>
     `;
-    Display.innerHTML = display;
+    display_div.innerHTML = display;
 }
 function fetchData(word) {
-    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=3ae00837-0e93-442b-8dd4-f3e64ced8c48`,
-        {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
-        }
+    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=3ae00837-0e93-442b-8dd4-f3e64ced8c48`
+        // {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' }
+        // }
     )
         .then(result => {
-            return result.json()
-
+            
+            return result.json();
+            
         })
         .then(datas => {
-            Display?.classList.add('invisible');
-            Display.innerHTML = '';
-            if (datas != '') {
-                let definitions = datas[0].shortdef;
-                if (definitions) {
-                    let word = datas[0].meta.id;
-                    let fl = datas[0].fl;
-                    let hw = datas[0].hwi.hw;
-                    let hwi = datas[0].hwi.prs;
-                    let ins = datas[0].ins
-                    let ins_l = parseInt(ins?.length) - 1;
-                    let new_prs = "| \\ "
-                    let new_ins = '';
-                    let new_def = '';
-
-                    if (datas[0].ins) {
-                        for (const [i, data] of ins.entries()) {
-                            if (data.il) {
-                                new_ins += ` <span class="if">${data.il.replace(/\*/g, '')};</span>`;
+            display_div.classList.add('invisible');
+            display_div.innerHTML = '';
+           
+                if (datas != '') {
+                    let definitions = datas[0].shortdef;
+                    if (definitions) {
+                        let word = datas[0].meta.id;
+                        let fl = datas[0].fl;
+                        let hw = datas[0].hwi.hw;
+                        let ins = datas[0].ins
+                        let new_prs = "| \\ "
+                        let new_ins = '';
+                        let new_def = '';
+                        if (datas[0].ins) {
+                            for (const [i, data] of ins.entries()) {
+                                if (data.il) {
+                                    new_ins += ` <span class="if">${data.il.replace(/\*/g, '')};</span>`;
+                                }
+                                if (i === parseInt(ins.length) - 1) {
+                                    new_ins += `<span class="if">${data.if.replace(/\*/g, '')}</span>`;
+                                } else {
+                                    new_ins += `<span class="if">${data.if.replace(/\*/g, '')};</span>`;
+                                }
+    
                             }
-                            if (i === ins_l) {
-                                new_ins += `<span class="if">${data.if.replace(/\*/g, '')}</span>`;
-                            } else {
-                                new_ins += `<span class="if">${data.if.replace(/\*/g, '')};</span>`;
+                        }
+                        for (const hw of datas[0].hwi.prs) {
+                            new_prs += `${hw.mw} `;
+                        }
+                        for (const [i, definition] of definitions.entries()) {
+                            new_def += `
+                            <div>
+                                  <span class="num">${parseInt(i) + 1}</span>
+                                    <span class="dtText">:${definition}</span>
+                                </div>
+                            `
+                        }
+                        if (datas[0].uros) {
+                            let new_other = '';
+                            for (const other of datas[0].uros) {
+                                new_other += `<span>${other.ure.replace(/\*/g, '')} <em>${other.fl.replace(/\*/g, '')} </em></span>`
                             }
-
+                            other_word.innerHTML += ` <h2 class="other_word">Other word for <em>${word}</em></h2>`;
+                            other_word.innerHTML += new_other;
+                        } else {
+                            other_word.innerHTML = '';
                         }
-                    }
-                    for (const hw of hwi) {
-                        new_prs += `${hw.mw} `;
-                    }
-                    for (const [i, definition] of definitions.entries()) {
-                        new_def += `
-                        <div>
-                              <span class="num">${parseInt(i) + 1}</span>
-                                <span class="dtText">:${definition}</span>
-                            </div>
-                        `
-                    }
-                    if (datas[0].uros) {
-                        let new_other = '';
-                        for (const other of datas[0].uros) {
-                            new_other += `<span>${other.ure.replace(/\*/g, '')} <em>${other.fl.replace(/\*/g, '')} </em></span>`
+                        if (datas[0].quotes) {
+                            let new_quotes = ''
+                            sentence.innerHTML += `<h2>Example of <em>${word}</em> in a Sentence</h2>`;
+                            for (const [i, quotes] of datas[0].quotes.entries()) {
+    
+                                new_quotes += `
+                            <div>
+                                  <span class="num">${parseInt(i) + 1}.</span>
+                                    <span class="dtText"> ${quotes.t}</span>
+                                </div>
+                            `
+                            }
+                            sentence.innerHTML += new_quotes.replace('{qword}', '<em>').replace('{/qword}', '</em>')
+                        } else {
+                            sentence.innerHTML = '';
                         }
-                        other_word.innerHTML += ` <h2 class="other_word">Other word for <em>${word}</em></h2>`;
-                        other_word.innerHTML += new_other;
+                        hword.innerHTML = `<h2>Definition of <em>${word}</em></h2>`;
+                        dom_fl.innerHTML = fl;
+                        word_syllables.innerHTML = hw;
+                        prs.innerHTML = `${new_prs} \\`;
+                        vg_ins.innerHTML = new_ins;
+                        def.innerHTML += `<em> ${word}</em>`;
+                        vg.innerHTML = new_def;
                     } else {
-                        other_word.innerHTML = '';
-                    }
-                    if (datas[0].quotes) {
-                        let new_quotes = ''
-                        sentence.innerHTML += `<h2>Example of <em>${word}</em> in a Sentence</h2>`;
-                        for (const [i, quotes] of datas[0].quotes.entries()) {
-
-                            new_quotes += `
-                        <div>
-                              <span class="num">${parseInt(i) + 1}.</span>
-                                <span class="dtText"> ${quotes.t}</span>
-                            </div>
-                        `
+                        let suggest = ` <div class="suggest">
+                        <h1 class="spacing">“${word}”</h1>
+                        <p class="spacing">The word you've entered isn't in the dictionary. Click on a spelling suggestion below or try again using the search bar above.</p>`;
+                        for (const data of datas) {
+                            suggest += `<span class='suggest_btn'>${data}</span>`;
                         }
-                        sentence.innerHTML += new_quotes.replace('{qword}', '<em>').replace('{/qword}', '</em>')
-                    } else {
-                        sentence.innerHTML = '';
-                    }
-                    hword.innerHTML = `<h2>Definition of <em>${word}</em></h2>`;
-                    dom_fl.innerHTML = fl;
-                    word_syllables.innerHTML = hw;
-                    prs.innerHTML = `${new_prs} \\`;
-                    vg_ins.innerHTML = new_ins;
-                    def.innerHTML += `<em> ${word}</em>`;
-                    vg.innerHTML = new_def;
-                } else {
-                    let suggest = '';
-                    for (const data of datas) {
-                        suggest += `
-                        <div class="suggest">
-                            <span class='suggest_btn'>${data}</span>
-                        </div>
-                        `;
-                    }
-                    def.innerHTML = suggest;
-                    const suggest_btn = document.querySelectorAll('.suggest_btn');
-                    console.log('False')
-                    if (suggest_btn) {
-                        for (const sug_btn of suggest_btn) {
-                            sug_btn.addEventListener('click', function () {
-                                clear();
-                                fetchData(this.innerHTML)
-                            });
+                        def.innerHTML = suggest+'</div>';
+                        const suggest_btn = document.querySelectorAll('.suggest_btn');
+                        if (suggest_btn) {
+                            for (const sug_btn of suggest_btn) {
+                                sug_btn.addEventListener('click', function () {
+                                    clear();
+                                    fetchData(this.innerHTML)
+                                });
+                            }
                         }
                     }
+    
                 }
-
-            }
+           
+           
             else {
                 clear();
                 errorDisplay(word);
@@ -174,8 +173,14 @@ function fetchData(word) {
         })
 
 }
-search_btn.addEventListener('click', () => {
-
+word_search.addEventListener('keyup',function(e){
+     e.preventDefault();
+    if(e.keyCode === 13) {
+        search_btn.click();
+    }
+   
+});
+search_btn.addEventListener('click',function(e){
     if (word_search.value !== '') {
         clear();
         loading();
@@ -183,7 +188,6 @@ search_btn.addEventListener('click', () => {
     } else {
         alert('You must input a word');
     }
-
 });
 clear_btn.addEventListener('click', function () {
     word_search.value = '';
